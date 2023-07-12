f268932f527551d23a7975f297d2498f8fcb6bba
Tushar Verma <tuhsar2022@gmail.com>
2023-07-12 19:37:28 +0530

    ui

--- src/components/Mainpart.js
+++ src/components/Mainpart.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { color, motion } from "framer-motion"
+import { color, motion } from "framer-motion";
 import wave from "../emojis/wave.png";
 import technologist from "../emojis/technologist.png";
 import pointRight from "../emojis/pointright.png";
@@ -12,9 +12,9 @@ import student from "../imges/student.png";
 import todo from "../imges/todo.png";
 import typer from "../imges/typer.png";
 import weather from "../imges/weather.png";
-import crypto from "../imges/crypto.png"
+import crypto from "../imges/crypto.png";
 import wp from "../imges/wp.png";
-import top from"../emojis/pointing-up.png";
+import top from "../emojis/pointing-up.png";
 import "../styles.css";
 import Projects from "./Projects";
 export default function Mainpart() {
@@ -27,20 +27,7 @@ export default function Mainpart() {
     "Bonjour!", // French
     "Ciao!", // Italian
     "Guten Tag!", // German
-    "Olá!", // Portuguese
-    "Здравствуйте!", // Russian
-    "مرحبا!", // Arabic
-    "Selamat pagi!", // Indonesian
-    "Merhaba!", // Turkish
-    "Hej!", // Swedish
-    "여보세요!", // Korean
-    "你好！", // Chinese
-    "Kia ora!", // Maori
-    "வணக்கம்!", // Tamil
-    "నమస్కారం!", // Telugu
-    "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ!", // Punjabi
-    "Γειά σας!", // Greek
-    "નમસ્તે!", // Gujarati
+   
   ];
 
   useEffect(() => {
@@ -53,9 +40,9 @@ export default function Mainpart() {
     };
   }, []);
 
-const handleScroll=()=>{
-  window.scrollTo({top:"0",behavior:"smooth"});
-}
+  const handleScroll = () => {
+    window.scrollTo({ top: "0", behavior: "smooth" });
+  };
 
   return (
     <div className="intro">
@@ -94,17 +81,29 @@ const handleScroll=()=>{
         <h2>BACKGROUND</h2>
         <div className="about">
           <p>
-          As a recent graduate with a Bachelor's degree in Computer Science, I am excited to bring my technical skills and passion for problem-solving to a dynamic and forward-thinking company.
-
-        </p>
-          <p>
-          During my studies, I gained a strong foundation in computer science concepts, including data structures, algorithms, and software development. I also worked on several personal projects, where I applied my skills to develop useful and innovative projects.
-
+            As a recent graduate with a Bachelor's degree in Computer Science, I
+            am excited to bring my technical skills and passion for
+            problem-solving to a dynamic and forward-thinking company.
           </p>
           <p>
-          I am a detail-oriented and proactive individual with excellent communication skills. I thrive in fast-paced environments and enjoy the challenge of tackling complex problems. In my spare time, I enjoy staying up-to-date with the latest technology trends and exploring new programming languages.
+            During my studies, I gained a strong foundation in computer science
+            concepts, including data structures, algorithms, and software
+            development. I also worked on several personal projects, where I
+            applied my skills to develop useful and innovative projects.
           </p>
-          <p>I am confident that my passion for technology and my drive to learn and grow will make me a valuable asset to any team. I am eager to begin my career and make a positive impact as a member of a talented and innovative team.</p>
+          <p>
+            I am a detail-oriented and proactive individual with excellent
+            communication skills. I thrive in fast-paced environments and enjoy
+            the challenge of tackling complex problems. In my spare time, I
+            enjoy staying up-to-date with the latest technology trends and
+            exploring new programming languages.
+          </p>
+          <p>
+            I am confident that my passion for technology and my drive to learn
+            and grow will make me a valuable asset to any team. I am eager to
+            begin my career and make a positive impact as a member of a talented
+            and innovative team.
+          </p>
         </div>
       </div>
       <div className="skills">
@@ -125,55 +124,98 @@ const handleScroll=()=>{
         </div>
       </div>
       <div className="resume">
-        <a href="https://drive.google.com/file/d/1Gj2cytJVJihutCc92eA_tXqCR7LBC_qc/view?usp=drive_link" style={{color:"white"}} target="blank">View My Resume</a>
+        <a
+          href="https://drive.google.com/file/d/1Gj2cytJVJihutCc92eA_tXqCR7LBC_qc/view?usp=drive_link"
+          style={{ color: "white" }}
+          target="blank"
+        >
+          View My Resume
+        </a>
       </div>
       <div className="projects">
         <h2>Featured Projects</h2>
-        <p style={{margin:"5rem",marginLeft:"9rem", textDecoration:"underline",fontSize:".9rem"}}>Open projects on clicking the any desired image and to close the project click on the navigation icons </p>
+        {/* <p className="instruction" >
+          Open projects on clicking the any desired image and to close the
+          project click on the navigation icons{" "}
+        </p> */}
         <div className="samples">
-      <div className="project-grid">
-        <div className="project">
-          <Projects imageSrc={typer} iframeSrc="https://super-typer.netlify.app/" />
-          <p>Typing Speed Test Using React</p>
+          <div className="project-grid">
+            <div className="project">
+              <Projects
+                imageSrc={typer}
+                iframeSrc="https://super-typer.netlify.app/"
+              />
+              <p>Typing Speed Test Using React</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={crypto}
+                iframeSrc="https://merry-bubblegum-919802.netlify.app/"
+              />
+              <p>Crypto Currency Price Tracker Using React</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={tesla}
+                iframeSrc="https://tesla-clone-main.netlify.app/"
+              />
+              <p>Tesla Home Page Clone Using React</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={post}
+                iframeSrc="https://1tushr.github.io/postOfficeLocator"
+              />
+              <p>Post Office Locator Using Ip Address Data Fetched From API</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={student}
+                iframeSrc="https://1tushr.github.io/StudentManagement/"
+              />
+              <p>Student Management CRUD App Using JS</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={todo}
+                iframeSrc="https://1tushr.github.io/todo"
+              />
+              <p>Todo App Using JS</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={weather}
+                iframeSrc="https://1tushr.github.io/weather/"
+                allow="geolocation"
+              />
+              <p>
+                Weather Checker Using Current Location Data Fetched From API
+              </p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={bmi}
+                iframeSrc="https://1tushr.github.io/bmi/"
+              />
+              <p>BMI Calculator</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={pizza}
+                iframeSrc="https://1tushr.github.io/Pizza-Restaurant/"
+              />
+              <p>Pizza Restaurant Landing Page</p>
+            </div>
+            <div className="project">
+              <Projects
+                imageSrc={wp}
+                iframeSrc="https://1tushr.github.io/wp-clone/"
+                title="WhatsApp clone"
+              />
+              <p>WhatsApp Landing Page Clone </p>
+            </div>
+          </div>
         </div>
-        <div className="project">
-          <Projects imageSrc={crypto} iframeSrc="https://merry-bubblegum-919802.netlify.app/" />
-          <p>Crypto Currency Price Tracker Using React</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={tesla} iframeSrc="https://tesla-clone-main.netlify.app/" />
-          <p>Tesla Home Page Clone Using React</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={post} iframeSrc="https://1tushr.github.io/postOfficeLocator" />
-          <p>Post Office Locator Using Ip Address Data Fetched From API</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={student} iframeSrc="https://1tushr.github.io/StudentManagement/" />
-          <p>Student Management CRUD App Using JS</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={todo} iframeSrc="https://1tushr.github.io/todo" />
-          <p>Todo App Using JS</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={weather} iframeSrc="https://1tushr.github.io/weather/" allow="geolocation" />
-          <p>Weather Checker Using Current Location Data Fetched From API</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={bmi} iframeSrc="https://1tushr.github.io/bmi/" />
-          <p>BMI Calculator</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={pizza} iframeSrc="https://1tushr.github.io/Pizza-Restaurant/" />
-          <p>Pizza Restaurant Landing Page</p>
-        </div>
-        <div className="project">
-          <Projects imageSrc={wp} iframeSrc="https://1tushr.github.io/wp-clone/" title="WhatsApp clone" />
-          <p>WhatsApp Landing Page Clone </p>
-        </div>
-      </div>
-    </div>
       </div>
       <footer className="footer">
         <div className="footer-content">
@@ -186,18 +228,28 @@ const handleScroll=()=>{
           <span>
             <p>by tushar</p>
           </span>
-          <div className="social">
-            <a href="https://www.linkedin.com/in/1tushr"target="_blank">LinkedIn</a>
-            <a href="mailto:tuhsar2022@gmail.com"target="_blank">Email</a>
-            <a href="https://github.com/1tushr"target="_blank">Github</a>
-            <img src={top} onClick={handleScroll} style={{width:"2rem"}}></img>
-          </div>
+        </div>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/1tushr" target="_blank">
+            LinkedIn
+          </a>
+          <a href="mailto:tuhsar2022@gmail.com" target="_blank">
+            Email
+          </a>
+          <a href="https://github.com/1tushr" target="_blank">
+            Github
+          </a>
+          <img
+            src={top}
+            onClick={handleScroll}
+            style={{ width: "2rem", margin: "1rem" }}
+          ></img>
         </div>
       </footer>
     </div>
   );
 }
 
-    // "@material-ui/core": "^4.12.4",
-    // "@mui/material": "^5.13.7",
-    // "@mui/styled-engine-sc": "^5.12.0",
+// "@material-ui/core": "^4.12.4",
+// "@mui/material": "^5.13.7",
+// "@mui/styled-engine-sc": "^5.12.0",
